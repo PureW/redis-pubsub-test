b753f634e246748c379792a77ff25655ab52bd09
Anders Bennehag <anders@bennehag.com>
2020-04-10 20:39:29 -0400

    Overflows with error:
    ```
    PUBL: Published 535400000 msgs (63Mb)
    failed reading pubsub message: Could not read enough bytes
    failed reading pubsub message: Could not read enough bytes
    failed reading pubsub message: Could not read enough bytes
    failed reading pubsub message: Could not read enough bytes
    failed reading pubsub message: Could not read enough bytes
    failed reading pubsub message: Could not read enough bytes
    PUBL: Published 535500000 msgs (63Mb)
    failed reading pubsub message: Could not read enough bytes
    failed reading pubsub message: Could not read enough bytes
    failed reading pubsub message: Could not read enough bytes
    PUBL: Published 535600000 msgs (63Mb)
    failed reading pubsub message: Could not read enough bytes
    failed reading pubsub message: Could not read enough bytes
    failed reading pubsub message: Could not read enough bytes
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,64 @@
 use redis::Commands;
 
+const REDIS_ENDPOINT: &'static str = "redis://localhost:6379";
 const PAYLOAD: &'static str = "bar bar bar bar";
 
 fn main() {
-    let redis_client = redis::Client::open("redis://localhost:6379/").unwrap();
-    let mut redis_conn = redis_client.get_connection().unwrap();
+    for _ in 0..5 {
+        run_publisher();
+    }
 
-    let pub_client = redis_client.clone();
+    // By removing this sleep, the program runs as expected since the
+    // consumer comes online first
+    //std::thread::sleep(std::time::Duration::from_millis(200));
+    run_subscriber();
+}
+
+fn run_publisher() {
+    let client = redis::Client::open(REDIS_ENDPOINT).unwrap();
     std::thread::spawn(move || {
-        let mut conn = pub_client.get_connection().unwrap();
+        let mut conn = client.get_connection().unwrap();
         // Set up a publisher that fills up subscriber buffers
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        let mut num_published = 0;
         loop {
-            let res: Result<(), _> = conn.publish("foo", PAYLOAD);
-            res.unwrap();
+            num_published += 1;
+            let res: Result<(), _> = conn.publish("foo", num_published);
+            if let Err(err) = res {
+                println!("ERROR: Publish failed {}", err);
+                match client.get_connection() {
+                    Ok(new_conn) => {
+                        conn = new_conn;
+                        println!("PUBL: Aquired a new connection");
+                    }
+                    Err(err) => {
+                        println!("ERROR: Could not acquire new conn: {}", err);
+                        std::thread::sleep(std::time::Duration::from_millis(100));
+                    }
+                }
+                continue;
+            }
+            if num_published % 100_000 == 0 {
+                println!(
+                    "PUBL: Published {} msgs ({}Mb)",
+                    num_published,
+                    num_published / std::mem::size_of::<u64>() / 1024 / 1024,
+                );
+            }
         }
     });
+}
 
-    let mut pubsub = redis_conn.as_pubsub();
+fn run_subscriber() {
+    let client = redis::Client::open(REDIS_ENDPOINT).unwrap();
+    let mut conn = client.get_connection().unwrap();
+    let mut pubsub = conn.as_pubsub();
     pubsub.subscribe("foo").unwrap();
     loop {
         // Subscribe to channel, but consume slower than publish
         match pubsub.get_message() {
             Ok(msg) => {
-                let msg_body: String = msg.get_payload().unwrap();
-                if msg_body != PAYLOAD {
-                    println!("Payload differs: {}", msg_body);
-                }
+                let _: u64 = msg.get_payload().unwrap();
             }
             Err(err) => {
                 println!("failed reading pubsub message: {}", err);
